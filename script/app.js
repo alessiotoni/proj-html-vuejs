e50a2ae79940de829411c1e643fa7ca6d99203a0
alessiotoni <toniellialessio@gmail.com>
2021-05-25 11:08:27 +0200

    completa lo slider

--- script/app.js
+++ script/app.js
@@ -72,6 +72,46 @@ const myApp = new Vue({
                 status: ""
             },
         ],
+        instructors: [
+            {
+                img: "../public/images/instructor-mikehart-400x254.jpg",
+                name: "mike hart",
+                description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            },
+            {
+                img: "../public/images/instructor-johnsmith-400x254.jpg",
+                name: "john smith",
+                description: "Lorem ipsum dolor sit amet consectetur adipisicing elit amet consectetur adipisicing elit.",
+            },
+            {
+                img: "../public/images/instructor-angelahart-400x254.jpg",
+                name: "angela hart",
+                description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia beatae reiciendis consectetur.",
+            },
+        ],
+        testimonials: [
+            {
+                img: "../public/images/testimonial-sophia.png",
+                description: "Lorem ipsum dolor sit amet consectetur adipisicing elit sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit sit amet consectetur adipisicing elit.",
+                name: "sophia jones",
+            },
+            {
+                img: "../public/images/testimonial-kelly.png",
+                description: "Lorem ipsum dolor sit amet consectetur adipisicing elit sit amet consectetur adipisicing elit. Lorem ipsum adipisicing elit. elit sit amet consectetur adipisicing elit. Lorem ipsum adipisicing elit.",
+                name: "kelly jones",
+            },
+            {
+                img: "../public/images/testimonial-kate.png",
+                description: "Lorem ipsum dolor sit amet consectetur adipisicing elit sit amet consectetur adipisicing elit. Lorem ipsum consectetur adipisicing elit.",
+                name: "kate jones",
+            },
+            {
+                img: "../public/images/testimonial-harold.png",
+                description: "Lorem ipsum dolor sit amet consectetur  elit sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur  elit sit amet consectetur adipisicing elit.",
+                name: "harold jones",
+            },
+        ],
+        testimonialActive: 0,
         showCard: 0,
     },
 
@@ -80,6 +120,9 @@ const myApp = new Vue({
         showLearnMore(index) {
             this.showCard = index
             console.log(index)
+        },
+        changeTestimonials(item, index) {
+            this.testimonialActive = index
         }
     },
 })
